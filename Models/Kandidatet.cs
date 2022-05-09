e84d0225e30797f2383036e05f6a92cbc197b9df
HamzHajra <hh51489@ubt-uni.net>
2022-05-09 23:55:45 +0200

    commit Kandidatet Model

---
+++ Models/Kandidatet.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace E_Voting.Models
+{
+    public class Kandidatet
+    {
+
+        public int KandidatiId { get; set; }
+        public string EmriKandidatit { get; set; }
+        public string MbiemriKandidatit { get; set; }
+        public int NumriRendor { get; set; }
+        public int NumriPersonal { get; set; }
+
+    }
+}
