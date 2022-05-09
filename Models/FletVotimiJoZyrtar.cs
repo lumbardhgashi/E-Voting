f5bac008bdfcb826ec4f30aa99a18ca3dc69addf
HamzHajra <hh51489@ubt-uni.net>
2022-05-10 00:07:40 +0200

    commit FletVotimiJoZyrtar Model

---
+++ Models/FletVotimiJoZyrtar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace E_Voting.Models
+{
+    public class FletVotimiJoZyrtar
+    { 
+        public int Id { get; set; }
+        public DateTime KohaStartuese { get; set; }
+        public DateTime KohaPerfunduese { get; set; }
+
+    }
+}
