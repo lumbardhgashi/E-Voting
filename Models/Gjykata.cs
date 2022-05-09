ad2f018b7f8f5a823423650326a13a9244c4138e
HamzHajra <hh51489@ubt-uni.net>
2022-05-09 23:48:16 +0200

    commit Gjykata Model

---
+++ Models/Gjykata.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace E_Voting.Models
+{
+    public class Gjykata
+    {
+
+        public int GjykataId { get; set; } 
+        public string GjykataQyteti{ get; set; }
+    }
+}
