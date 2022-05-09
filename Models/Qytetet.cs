7f12b23e7b01d45a08870cb1d86b5196764ca731
HamzHajra <hh51489@ubt-uni.net>
2022-05-09 23:57:03 +0200

    commit Qytetet Model

---
+++ Models/Qytetet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace E_Voting.Models
+{
+    public class Qytetet
+    {
+
+        public int QytetiId { get; set; }
+        public string EmriQytetit { get; set; }
+        public int PopullsiaQytetit { get; set; }
+        public string KryetariQytetit { get; set; }
+
+    }
+}
