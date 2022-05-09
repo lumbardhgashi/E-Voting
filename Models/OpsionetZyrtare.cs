a528a32897deb33263cc5e1965f12a76bc47369d
HamzHajra <hh51489@ubt-uni.net>
2022-05-09 23:58:34 +0200

    commit OpsionetZyrtare Model

---
+++ Models/OpsionetZyrtare.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Voting.Models
+{
+    public class OpsionetZyrtare
+    {
+      public int Id { get; set; }   
+      public string Opsioni  { get; set; }
+
+    }
+}
