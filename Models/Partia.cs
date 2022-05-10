9e0c3ddd5b37b9c1a125ac77b5aa18fea0fe8e5e
Xon1 <ep52768@ubt-uni.net>
2022-05-11 00:41:48 +0200

    commit partia

---
+++ Models/Partia.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Voting.Models
+{
+    public class Partia
+    {
+
+        public int PartiaId { get; set; }
+
+        public string EmriPartis { get; set; }
+
+        public string KryetariPartis { get; set; }
+
+        public int NumriAntareve { get; set; }
+
+        public int NumriRendorIpartis { get; set; }
+    }
+}
