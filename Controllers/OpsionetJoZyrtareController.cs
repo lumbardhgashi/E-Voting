22a1ff6109cea4eda0cd35e5d2340d510392cb34
Xon1 <ep52768@ubt-uni.net>
2022-05-11 00:47:08 +0200

    commit opsionetJoZyrtare

---
+++ Controllers/OpsionetJoZyrtareController.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using E_Voting.Models;
+
+namespace E_Voting.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OpsionetJoZyrtareController : ControllerBase
+    {
+
+        private readonly IConfiguration _configuration;
+
+        public OpsionetJoZyrtareController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+
+        public JsonResult Get()
+        {
+            string query = @"
+                              select * from dbo.OpsionetJoZyrtare;
+                                ";
+
+            DataTable table = new DataTable();
+            string dboDataSource = _configuration.GetConnectionString("E-VotingAppCon");
+            SqlDataReader myReader;
+
+            using (SqlConnection myCon = new SqlConnection(dboDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+
+        public JsonResult Post(OpsionetJoZyrtare ojz)
+        {
+            string query = @" insert into dbo.OpsionetJoZyrtare values(@Opsioni)         
+                                  ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("E-VotingAppCon");
+            SqlDataReader myReader;
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@Opsioni", ojz.Opsioni);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("inserted successfully!");
+        }
+
+
+        [HttpPut]
+
+        public JsonResult Put(OpsionetJoZyrtare ojz)
+        {
+            string query = @" update dbo.OpsionetJoZyrtare set Opsioni = @Opsioni where Id = @Id";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("E-VotingAppCon");
+            SqlDataReader myReader;
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+
+                    myCommand.Parameters.AddWithValue("@Id", ojz.Id);
+                    myCommand.Parameters.AddWithValue("@Opsioni", ojz.Opsioni);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Updated successfully!");
+        }
+
+        [HttpDelete("{Id}")]
+
+        public JsonResult Delete(int id)
+        {
+            string query = @" delete dbo.OpsionetJoZyrtare where Id = @Id";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("E-VotingAppCon");
+            SqlDataReader myReader;
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@Id", id);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+
+                }
+            }
+            return new JsonResult("Delete Successfully!");
+        }
+
+    }
+}
+
