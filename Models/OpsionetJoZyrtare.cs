55f3e9949c15cdb5405cfcd3a8b52ad21e2fd785
Xon1 <ep52768@ubt-uni.net>
2022-05-11 00:43:24 +0200

    commit opsionetJoZyrtare

---
+++ Models/OpsionetJoZyrtare.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Voting.Models
+{
+    public class OpsionetJoZyrtare
+    {
+
+        public int Id { get; set; }
+
+        public string Opsioni { get; set; }
+    }
+}
