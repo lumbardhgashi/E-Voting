47ec61452d4e39293dde86f9d631486e8d6d99a4
Xon1 <ep52768@ubt-uni.net>
2022-05-11 00:31:34 +0200

    commit shtetimodel

---
+++ Models/NumriTelefonit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Voting.Models
+{
+    public class NumriTelefonit
+    {
+
+        public int NrTelId { get; set; }
+
+        public int NrTelefonit { get; set; }
+    }
+}
