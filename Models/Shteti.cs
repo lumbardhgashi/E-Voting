1fcc4a907d896c414793237090079f675c7bff32
Xon1 <ep52768@ubt-uni.net>
2022-05-10 23:17:59 +0200

    commit shtetimodel

---
+++ Models/Shteti.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Voting.Models
+{
+    public class Shteti
+    {
+
+        public int ShtetiId { get; set; }
+
+        public string ShtetiEmri { get; set; }
+
+        public string Kryeministri { get; set; }
+
+        public int NrPopullsis { get; set; }
+
+        public int NrQyteteve { get; set; }
+
+        public string OrganizataPerZgjedhje { get; set; }
+        
+    }
+}
